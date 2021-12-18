0cd2ed6976945b88093636aef970f8b001a97168
agrim <agrim.tuladhar@gmail.com>
2021-12-18 11:18:13 +0545

    Final Design Project Rechecked

--- docs/Design/Assignment4/js/index.js
+++ docs/Design/Assignment4/js/index.js
@@ -2,6 +2,10 @@ let navControl = document.querySelector(".hamburger");
 let smallNav = document.querySelector(".navlinks-smallscreen");
 
 navControl.addEventListener("click",()=>{
+  toggleNav();
+})
+
+function toggleNav(){
   navControl.classList.toggle("cross");
   smallNav.classList.toggle("nav-active");
-})
+}
